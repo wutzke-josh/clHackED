4870bdf2cf8781000d7e1a08a475aaee94e52052
JMacKenzie872 <57580342+JMacKenzie872@users.noreply.github.com>
2020-01-18 14:20:18 -0700

    A small beginning

--- clHackED.cpp
+++ clHackED.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-	int integer;
-
-	cout << "Enter an integer please:" << endl;
-
-	cin >> integer;
-
-	if(integer == 69) {
-		cout << "nice.";
-	} 
-	else {
-		cout << "Bad choice.";
-	}
-
-	cout << endl;
-	return 0;
+int phase = 0;
+void setup(){
+    cout << "Welcome to the Command Line HackED terminal Hacking game \n" ;
+    cout << "Please press 1 to Continue \n"; 
 }
+void loop(){
+    if(phase == 0){
+        int input = 0;
+        try{
+            cin >> input;
+        }catch( exception& e){
+            cout << "Invalid Input: Please try again \n";
+        }
+        if(input == 1){
+            phase = 1;
+        }else{
+            cout << "Invalid Input: Please try again \n";
+            input = 0;
+        }
+    }
+}
+
+int main(){
+    setup();
+    while(true){
+        loop();
+    }
+    return 0;
+}   
