e0c5d9fe20b2fec0cba25defa5a7e667663ffd30
wutzke-josh <wutzke.josh@gmail.com>
2020-01-18 14:15:28 -0700

    initializing main file

---
+++ clHackED.cpp
@@ -0,0 +1,21 @@
+#include <istream>
+
+using namespace std;
+
+int main() {
+	int integer;
+
+	cout << "Enter an integer please:" << endl;
+
+	cin >> integer;
+
+	if(int == 69) {
+		cout << "nice.";
+	} 
+	else {
+		cout << "Bad choice.";
+	}
+
+	cout << endl;
+	return 0;
+}
